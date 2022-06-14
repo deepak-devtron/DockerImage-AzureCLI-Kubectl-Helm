9d02e0a86dc8e908c01bf1b9c31e90b81370e3e5
deepak-devtron <devtron@devtrons-MacBook-Pro.local>
2022-06-14 13:16:07 +0530

    adding logs in python script

--- az-cluster-create.py
+++ az-cluster-create.py
@@ -1,11 +1,20 @@
 import argparse
 import os
 import time
+import subprocess as sp
+
+
+def banner(heading):
+    print("-" * 35)
+    print(heading)
+    print("-" * 35)
+
 
 parser = argparse.ArgumentParser(prog='python3 az-cluster-create.py', usage='%(prog)s [options]',
                                  description="A Script Written in Python to Create AKS cluster")
 
-# Here we are storing the values of arguments the user is passing with the command of "python run"
+banner("Here we are storing the values of arguments that the user is passing with the command of 'python run'")
+
 parser.add_argument("-u", "--username", required=True, help="service principal username", metavar="")
 parser.add_argument("-p", "--password", required=True, help="service principal password", metavar="")
 parser.add_argument("-t", "--tenant", required=True, help="service principal tenant", metavar="")
@@ -14,29 +23,44 @@ parser.add_argument("-c", "--clusterName", required=True, help="service principa
 
 args = parser.parse_args()
 
-# Here we are login aws account having MFA enabled
+banner("Here we are login aws account having MFA enabled")
 os.system(f"az login --service-principal -u {args.username} -p {args.password} --tenant {args.tenant}")
 
-# Here we're creating ResourceGroup
+banner("Here we're creating ResourceGroup")
 os.system(f"az group create --name {args.resourceGroup} --location eastus")
-time.sleep(10)
+time.sleep(5)
 
-# Here we are creating AKS cluster
-os.system(
-    f"az aks create --resource-group {args.resourceGroup} --name {args.clusterName} --node-count 1 --generate-ssh-keys --enable-node-public-ip")
-time.sleep(600)
-# Here we are getting aks credentials
+banner("Here we are creating AKS cluster")
+os.system(f"az aks create --resource-group {args.resourceGroup} --name {args.clusterName} --node-count 1 --generate-ssh-keys --enable-node-public-ip")
+print("cluster creation complete..")
+
+banner("Here we are getting aks credentials")
 os.system(f"az aks get-credentials --resource-group {args.resourceGroup}  --name {args.clusterName}")
 
-# Here we are creating repo for Devtron installation
+banner("Here we are installing Devtron in our freshly created cluster")
+sp.getoutput("wget https://raw.githubusercontent.com/prakarsh-dt/kops-cluster/main/devCluster/devtron-ucid.yaml")
+os.system("kubectl create namespace devtroncd")
+os.system("kubectl apply -f devtron-ucid.yaml -ndevtroncd")
+banner("devtron-ucid has been applied in devtroncd ns")
+os.system("rm devtron-ucid.yaml")
 os.system("helm repo add devtron https://helm.devtron.ai/")
+os.system("helm install devtron devtron/devtron-operator --namespace devtroncd --set installer.modules={cicd}")
 
-# Here we are installing Devtron on our freshly created cluster
-os.system("helm install devtron devtron/devtron-operator --create-namespace --namespace devtroncd")
-time.sleep(1200)
+banner("Here we are checking installation status of application")
+os.system("sleep 2")
+os.system("touch status.txt")
 
-# Here we are getting all the pods in devtroncd namespace
-os.system("kubectl get po -ndevtroncd")
+while sp.getoutput("cat status.txt") != "Applied":
+    sp.getoutput("kubectl -n devtroncd get installers installer-devtron -o jsonpath='{.status.sync.status}' > status.txt")
+    os.system("sleep 30")
+    if sp.getoutput("cat status.txt") == "Downloaded":
+        print("\nStill Downloading Microservices : ", end="")
+        os.system("cat status.txt")
+        print("\n")
+        os.system("kubectl get pods -ndevtroncd")
+    else:
+        continue
+print("devtron installation complete..")
 
-# Here we are deleting azure resource group
+banner("Here we are deleting azure resource group")
 os.system(f"az group delete -y --name {args.resourceGroup}")
